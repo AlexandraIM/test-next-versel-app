bfa2b651d5053451ea5236bf1c9b94075aba6d86
Oleksandra Omer-Imereli <o.omer-imereli@globallogic.com>
2025-02-24 14:39:42 +0200

    fix all errrs for build

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,13 +1,15 @@
-import Post from "@/app/ui/components/posts/Post";
-import { posts } from "@/app/lib/placeholder-data";
-import { Post as PostType } from "@/app/lib/definitions";
+import { posts } from '@/app/lib/placeholder-data';
+import Post from '@/app/ui/components/posts/Post';
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
 
-export default function Page({params}: {params: {id: string}}) {
-  const post = posts.find((post) => post.id === params.id);
+export default async function Page({ params }: PageProps) {
+  const resolvedParams = await params;
+  const post = posts.find((post) => post.id === resolvedParams.id);
   return (
     <>
       <h1>Post</h1>
-      <Post {...post as PostType}/>
-    </>
-  );
+      <Post {...post} />
+    </>)
 }
--- src/app/ui/components/posts/Post.tsx
+++ src/app/ui/components/posts/Post.tsx
@@ -1,11 +1,17 @@
-import Link from "next/link"
+import React from 'react';
+interface PostProps {
+  id?: string;
+  title?: string;
+  content?: string;
+  date?: string;
+}
 
-export default function Component({ id, title, content, date }: { id: string, title: string, content: string, date: string }) {
-  return (
-      <div key={id} className="border border-gray-200 p-4 my-4">
-          <Link href={`/blog/post/${id}`}><h2>{title}</h2></Link>
-          <p className="text-gray-500">{date}</p>
-          <p>{content}</p>
-      </div>
-  );
+export default function Component({ id, title, content, date }: PostProps) {
+    return (
+        <div key={id} className="border border-gray-200 p-4 my-4">
+            <h2>{title}</h2>
+            <p className="text-gray-500">{date}</p>
+            <p>{content}</p>
+        </div>
+    );
 }
